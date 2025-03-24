eaa590cb493d1eaef4e20ed2273b9158117ac0a8
christophercodinga11y <christopherjack14@gmail.com>
2025-03-24 15:14:26 -0500

    working on the challenges

--- my-data.sql
+++ my-data.sql
@@ -14,6 +14,7 @@ select first_name, last_name title
 from employee
 where salary between 40000 and 80000;
 
+-- Return the total number of products sold
 select sum(products_sold), count(*)
 from transaction;
 
@@ -26,5 +27,30 @@ select employee_id, sum(amount) tot_amt, count(*)
 from transaction
 group by employee_id;
 
---  Return the total transaction amount on March 19, 2025
-select sum
+--  Return the total transaction amount on March 19, 2025 
+select sum(amount) tot_amt, count(*)
+from transaction
+where transaction_date = '2025-03-19';
+
+-- Return the total contract amount
+select sum(amount) tot_amt
+from contract;
+
+-- Return the total contract amount for each employee
+select employee_id, sum(amount) tot_amt
+from contract
+group by employee_id;
+
+
+-- Return the employee name for each contract
+select client
+from contract
+group by client;
+
+-- Return each contract that will be completed after Dec 31, 2025
+select contract_id, employee_id
+from contract
+where 
+
+-- Return the transaction or contract sale for each employee. If an employee did not have a sell Display 'N/A'
+
